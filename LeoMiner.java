a53f512571da1968b93af9a49826fd5ae1d959af
Stephen Rodgers <xionanx@hotmail.com>
2013-04-16 21:33:53 -0400

    Changing the laser block

--- LeoMiner.java
+++ LeoMiner.java
@@ -2,9 +2,12 @@ package leominer;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraftforge.common.Configuration;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
@@ -16,12 +19,28 @@ public class LeoMiner
     public static final String modid = "Xionanx_LeoMiner";
 
     public static Block leolaserBlock;
+    public static int leolaserBlockID;
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+            Configuration config = new Configuration(event.getSuggestedConfigurationFile());
 
+            config.load();
+
+            int leolaserBlockID = config.getBlock("leolaserBlock", 500).getInt();       
+
+            config.save();
+    }
+
+    
     @Init
     public void load(FMLInitializationEvent event)
     {
-        leolaserBlock = new BlockLEOLaserBlock(500, Material.rock).setUnlocalizedName("leolaserBlock");
+        leolaserBlock = new LEOLaserBlock(leolaserBlockID, Material.cactus).setUnlocalizedName("leolaserBlock");
         GameRegistry.registerBlock(leolaserBlock, modid + leolaserBlock.getUnlocalizedName2());
         LanguageRegistry.addName(leolaserBlock, "LEO Laser Block");
     }
+    
+    
+   
 }
